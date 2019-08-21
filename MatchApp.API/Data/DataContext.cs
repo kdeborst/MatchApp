f15f3ed64c78fea01e72985179f0bd9db4c1097b
Kevin de Borst <kevin.de.borst@gmail.com>
2019-08-21 15:34:37 +0200

    Initial Commit

---
+++ MatchApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+// ASSEMBLY REFERENCES
+using MatchApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ MatchApp.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace MatchApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
