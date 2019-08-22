12d5b4804fa8c3b197676a08facb4af8393394a2
Kevin de Borst <kevin.de.borst@gmail.com>
2019-08-22 14:07:17 +0200

    Added Login/Register Components & Connected to API

---
+++ MatchApp-SPA/src/app/navigation/navigation.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/Auth.service';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+
+export class NavigationComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('Failed to login');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged out successfully');
+  }
+}
